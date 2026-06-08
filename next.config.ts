4d1dcb3d1356df00d6b20f414f0d2fc2cfbc1546
Vidhi Desai <vidhi@vidhidesai.com>
2026-06-08 22:15:08 +0100

    Add Next redirect for lokantara.space

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        destination: "https://lokantaraspace.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
