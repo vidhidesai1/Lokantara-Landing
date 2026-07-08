4432a8b8202d6a1f2e4692e524131cda1cccb887
Vidhi Desai <vidhi@vidhidesai.com>
2026-07-08 01:00:16 +0100

    Revert "Add Next redirect for lokantara.space"
    This reverts commit 4d1dcb3d1356df00d6b20f414f0d2fc2cfbc1546.

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        destination: "https://lokantaraspace.com/:path*",
-        permanent: true,
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
