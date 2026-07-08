8c38c16d592f1fd510c5cc41b4a9e230c953a179
Vidhi Desai <vidhi@vidhidesai.com>
2026-07-08 01:00:16 +0100

    Revert "Show maintenance page for lokantara.space"
    This reverts commit e41f6165d62909907e875235ab3edc6e897696b8.

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,15 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        destination: "https://lokantaraspace.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
